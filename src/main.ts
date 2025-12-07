9fd894653d237222e42027c99d8916a4d234876c
Arief Hakim <arief88luqman@gmail.com>
2025-12-07 19:24:43 +0700

    fix(Resolve CVE Issue and Migrate into pnpm): "Fix cve issue and migrate into pnpm"

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
